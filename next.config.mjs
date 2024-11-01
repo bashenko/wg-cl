574209de5a3acb78407989a7e06edfd6678fd9ea
Konstantin Bashenko <konstantin@bashenko.com>
2024-11-01 10:50:07 +0100

    t

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'wgs-directus.159.89.105.47.sslip.io',
+        hostname: process.env.NEXT_PUBLIC_DIRECTUS_API_URL,
         port: '8055', // Port where Directus is accessible
         pathname: '/assets/**', // Allow all assets from the /assets path
       },
@@ -15,7 +15,7 @@ const nextConfig = {
         source: "/items/:path*", // Applies to all /items/ API routes
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://qoogssgks8800o0soskggcw4.159.89.105.47.sslip.io" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
