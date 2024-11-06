b6f6acde4dffd2c85a5b73d7f5383df7bfb66311
Konstantin Bashenko <konstantin@bashenko.com>
2024-11-06 15:51:44 +0100

    main cache

--- app/api/proxy/main_page/route.js
+++ app/api/proxy/main_page/route.js
@@ -2,9 +2,24 @@ import axios from 'axios';
 
 export const GET = async () => {
   try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_DIRECTUS_API_URL}/items/Main_Page`);
-    return new Response(JSON.stringify(response.data), { status: 200 });
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_DIRECTUS_API_URL}/items/Main_Page`, {
+      headers: {
+        'Cache-Control': 'no-store', // Ensure no caching for each request
+      },
+    });
+
+    return new Response(JSON.stringify(response.data), {
+      status: 200,
+      headers: {
+        'Cache-Control': 'no-store, must-revalidate', // Prevent caching in response
+      },
+    });
   } catch (error) {
-    return new Response(JSON.stringify({ message: 'Error fetching Main_Page data' }), { status: 500 });
+    return new Response(JSON.stringify({ message: 'Error fetching Main_Page data' }), {
+      status: 500,
+      headers: {
+        'Cache-Control': 'no-store, must-revalidate', // Prevent caching even for errors
+      },
+    });
   }
 };
